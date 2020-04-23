ae9c3135235cc071c90fc7fdb04bd9b7b47a0907
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-04-23 16:02:43 +0100

    completed mvp & some extensions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+ <div>
+   <h1>Brewdog Beers</h1>
+   <div class="main-container">
+     <beer-list :beers='beers'></beer-list>
+     <beer-detail v-if='selectedBeer' :beer='selectedBeer'></beer-detail>
+     <button v-on:click='addFavorite'>Add Favorite</button>
+     <button v-if='favoriteBeers.includes(selectedBeer)'>Delete Favorite</button>
+      <div class='favorite-beers-list'>
+        <h2>Favorite Beers</h2>
+        <beer-list :beers='favoriteBeers'></beer-list>
+      </div>
+   </div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BeerList from './components/BeerList.vue';
+import {eventBus} from './main.js';
+import BeerDetail from './components/BeerDetail.vue';
+
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      beers: [],
+      selectedBeer: null,
+      favoriteBeers: []
+    };
+  },
+
+  mounted (){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(result => result.json())
+    .then(beers => this.beers = beers)
+
+    eventBus.$on('beer-selected', (beer) => {
+      this.selectedBeer = beer
+    })
+  },
+  methods: {
+    addFavorite: function() {
+      if (this.favoriteBeers.includes(this.selectedBeer) === false) {
+        this.favoriteBeers.push(this.selectedBeer);
+      }
+    }
+  },
   components: {
-    HelloWorld
+    'beer-list': BeerList,
+    'beer-detail': BeerDetail,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/BeerDetail.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <h3>{{beer.name}} ABV:{{beer.abv}}</h3>
+    <p>Description: {{beer.description}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'beer-detail',
+  props: ['beer']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
